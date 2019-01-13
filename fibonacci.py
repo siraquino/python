4d0b334446394acc496132ea5f8301547b589de7
Rafael AQuino <46247548+siraquino@users.noreply.github.com>
2019-01-13 18:29:30 -0400

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+print("#Cantidad 1 2 3 4 5 6 7 8  9  10")
+print("#Serie F. 0 1 1 2 3 5 8 13 21 34")
+x=0
+y=1
+c=0
+con=0
+print(x)
+print(y)
+while (con<10-2):
+    c=x+y
+    print(c)
+    x=y
+    y=c
+    con=con+1
+print("@siraquino")
+print("Fin del Programa")
