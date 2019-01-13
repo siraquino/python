a19e2bfe157ed02129e189d5852626fbb6d4655d
Rafael AQuino <46247548+siraquino@users.noreply.github.com>
2019-01-13 18:12:07 -0400

    Add files via upload

---
+++ escaques_ajedrez.py
@@ -0,0 +1,11 @@
+print("Ajedrez - Nombres de los escaques")
+print("para la notación algebraica")
+
+for i in range(8,0,-1):
+    print()
+    print()
+    for j in 'abcdefgh':
+        print(j,i," ", end=" ")
+print()
+print("@siraquino")
+print ("Fin d el programa")
