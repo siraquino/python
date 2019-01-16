c66c0f10ed6f09a239ff1cad9148fe1ee236eb6e
Rafael AQuino <46247548+siraquino@users.noreply.github.com>
2019-01-16 07:42:51 -0400

    Add files via upload

---
+++ sumardosnumeros.py
@@ -0,0 +1,45 @@
+print("Hola Mundo")
+# Así comenzaba la clase de algoritmos con Qbasic y Pascal,
+# de mis cursos para iniciados, en este maravilloso mundo de la programazción.
+# Ahora yo los hago iniciándome en Python...
+
+
+#......................................................
+# Ejercicio N1 sumar a y b
+print("Programa que suma el numero 12 y el numero 23")
+a = 12
+b = 23
+suma = a + b
+print("La Suma es: ", suma)
+
+#Segunda forma de solución
+print("Programa que suma el numero 12 y el numero 23")
+a = 12
+b = 23
+print("La Suma es: ", a+b)
+
+#Tercera forma de solución
+print("Programa que suma el numero 12 y el numero 23")
+a = 12
+b = 23
+print("La Suma es: ", b+a)
+#........................................................
+
+
+# Ejercicio N2 sumar 12 y un número introducido por teclado
+print("Programa que suma el numero 12 y otro numero")
+a = 12
+b = int(input("Introduzca el otro valor positivo"))
+suma = a + b
+print("La Suma es: ", suma)
+
+#............................................................
+
+# Ejercicio N3 sumar dos numeros introducidos por teclado
+print("Programa que suma Dos numero positivos")
+a = int(input("Introduzca el primer valor positivo: "))
+b = int(input("Introduzca el segundo valor positivo: "))
+suma = a + b
+print("La Suma es: ", suma)
+
+print("siraquino")
