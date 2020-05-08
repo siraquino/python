fb7f7f985b897fbd8de697984e857b98bb0a98bd
Rafael AQuino <46247548+siraquino@users.noreply.github.com>
2020-05-08 08:51:43 -0400

    operadores

---
+++ ejercicios_python_cumanes/operadores.py
@@ -0,0 +1,34 @@
+# El comentario comienza con el signo # 
+
+print("El comentario comienza con el signo #") 
+print
+print("Estudio de los operadores aritméticos ") 
+
+num1 = 23
+num2 = 12
+
+print("Número 1: ", num1)
+print("Número 2: ", num2)
+
+# operador  +
+print("Suma: ", num1 + num2)
+
+# operador -
+print("Resta: ", num1 - num2)
+
+# operador * multiplicación
+print("Multiplicación: ", num1 * num2) # esto es un comentario
+
+# operador ** exponenciación
+print("Exponenciación: ", 2 ** 3) 
+
+#print(num1 / num2) esto tambien es un comentario la función print() no se ejecuta 
+
+# operador / División 
+print("División:", num1 / num2 ) # Esta instrucción si se ejecuta
+
+# operador // Cociente de la división entera (floor division)
+print("floor división: ", num1 // num2)
+
+# operador % Resto de la división entera (modulus)
+print("Resto ", num1 % num2)
