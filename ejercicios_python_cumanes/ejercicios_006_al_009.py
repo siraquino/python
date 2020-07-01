acab8713393ef754bec49b017b66e4189dc3fa3f
Rafael AQuino <46247548+siraquino@users.noreply.github.com>
2020-07-01 19:10:56 -0400

    Create ejercicios_006_al_009.py
    Subiendo nuevos ejerecicios del 6 al 9

---
+++ ejercicios_python_cumanes/ejercicios_006_al_009.py
@@ -0,0 +1,56 @@
+# Ejercicios resueltos del 006 al 009
+
+# En el ejercicio 6 y 7 trabajaremos con "La División Entera" 
+# Se refiere al cociente de la división donde devuelve la parte entera de la división.
+
+#......................................................
+# Ejercicio resuelto N006
+# programa: Se desea saber la división entera al dividir 20 entre 6
+# 
+print("Programa N006: Calcular la División entera al dividir 20 entre 6")
+dividendo = 20
+divisor = 6
+cociente = dividendo // divisor
+print("La división entera al dividir 20 entre 6 es:",cociente)
+print()
+#...................................................
+
+# Ejercicio resuelto N007
+# programa: Se desea saber la división entera al dividir 3 entre 2
+# 
+print("Programa N007: Calcular la División entera al dividir 3 entre 2")
+dividendo = 3
+divisor = 2
+cociente = dividendo // divisor
+print("La división entera al dividir: ", dividendo, "entre ",divisor,"es: ", cociente)
+print()
+#...................................................
+
+# En el ejercicio 8 y 9 trabajaremos con el "Resto" de la división.
+# También es conocido como "Residuo" y en los lenguajes de programación
+# es llamado "Módulo"
+
+
+# Ejercicio resuelto N008
+# programa: Se desea saber el resto de la división: 20 entre 6
+# 
+print("Programa N008: Calcular el resto al dividir 20 entre 6")
+dividendo = 20
+divisor = 6
+resto = dividendo % divisor
+print("El Resto es: ",resto)
+print()
+#...................................................
+# Ejercicio resuelto N009
+# programa: Se desea saber el resto de la división: 3 entre 2
+# 
+print("Programa N009: Calcular el resto al dividir 3 entre 2")
+dividendo = 3
+divisor = 2
+resto = dividendo % divisor
+print("El resto al dividir: ", dividendo, "entre ",divisor,"es: ", resto)
+print()
+
+#........................................................
+# Primero cópialos como están escritos, luego intenta modificar sus valores para practicar
+#Rafael Aquino / mayo-2020 / pythoncumanes
