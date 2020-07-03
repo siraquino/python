9f596c42daf9836ebf47bc551c67c2f11d7c639b
Rafael AQuino <46247548+siraquino@users.noreply.github.com>
2020-07-03 12:59:33 -0400

    Create ejercicios_010_al_021.py
    Ejercicios

---
+++ ejercicios_python_cumanes/ejercicios_010_al_021.py
@@ -0,0 +1,142 @@
+# Ejercicios resueltos del 010 al 021
+
+# Los ejercicios del 010 al 021 son de potenciación
+#......................................................
+# Ejercicio resuelto N010
+# programa: Se desea saber cuanto es 2 elevado a la 3
+# 
+print("Programa N010: Calcular cuánto es 2 elevado a la 3")
+base = 2
+exponente = 3
+potencia = 2 ** 3
+print("2 elevado a la 3 es: ",potencia)
+print()
+#...................................................
+
+# Ejercicio resuelto N011
+# programa: Se desea saber cuanto es 3 elevado a la 2
+# 
+print("Programa N011: Calcular cuánto es 3 elevado a la 2")
+numero = 3
+elevado = 2
+resultado = numero ** elevado
+print(numero, "elevado a la : ", elevado, "es igual a: ", resultado)
+print()
+#...................................................
+
+# Ejercicio resuelto N012
+# programa: Se desea saber el cuadrado del número 5
+# 
+print("Programa N012: Calcular el cuadrado del número 5")
+num = 5
+a_la_dos = 2
+cuadrado = num ** a_la_dos
+print(num, "elevado a la : ", a_la_dos, "es igual a: ", cuadrado)
+print()
+#...................................................
+
+# Ejercicio resuelto N013
+# programa: Se desea saber el cubo de 2
+# 
+print("Programa N013: Calcular el cubo de 2")
+valor = 2
+a_la_tres = 3
+cubo = valor ** a_la_tres
+print(valor, "elevado a la : ", a_la_tres, "es igual a: ", cubo)
+print()
+#...................................................
+
+# Ejercicio resuelto N014
+# programa: Se desea saber el cubo de 3
+# 
+print("Programa N014: Calcular el cubo de 2")
+valor = 3
+a_la_tres = 3
+cubo = valor ** a_la_tres
+print(valor, "elevado a la : ", a_la_tres, "es igual a: ", cubo)
+print()
+#...................................................
+
+# Ejercicio resuelto N015
+# programa: Se desea saber cuanto es 10 elevado a la 5 
+# 
+print("Programa N015: Calcular 10 elevado a la 5")
+base = 10
+expo = 5
+resultado = base ** expo
+print(base, "elevado a la : ", expo, "es igual a: ", resultado)
+print()
+#...................................................
+
+# Ejercicio resuelto N016
+# programa: Se desea saber cuanto es -1 elevado a 2
+# 
+print("Programa N016: Calcular -1 elevado a la 2")
+numero = -1
+elevado = 2
+resultado = numero ** elevado
+print(numero, "elevado a la : ", elevado, "es igual a: ", resultado)
+print()
+#...................................................
+
+# Ejercicio resuelto N017
+# programa: Se desea saber cuanto es -1 elevado a 4
+# 
+print("Programa N017: Calcular -1 elevado a la 4")
+numero = -1
+elevado = 4
+resultado = numero ** elevado
+print(numero, "elevado a la : ", elevado, "es igual a: ", resultado)
+print('Hay un detalle cuando elevo el -1 a un numero par')
+print()
+#...................................................
+
+# Ejercicio resuelto N018
+# programa: Se desea saber cuanto es -1 elevado a 3
+# 
+print("Programa N018: Calcular -1 elevado a la 3")
+numero = -1
+elevado = 3
+resultado = numero ** elevado
+print(numero, "elevado a la : ", elevado, "es igual a: ", resultado)
+print()
+#........................................................
+# Ejercicio resuelto N019
+# programa: Se desea saber cuanto es -1 elevado a 17
+# 
+print("Programa N019: Calcular -1 elevado a la 17")
+numero = -1
+elevado = 17
+resultado = numero ** elevado
+print(numero, "elevado a la : ", elevado, "es igual a: ", resultado)
+print('Hay un detalle cuando elevo el -1 a un numero impar')
+
+print()
+#........................................................
+# Ejercicio resuelto N020
+# programa: Se desea saber cuanto es 101010 elevado a la 0
+# 
+print("Programa N020: Calcular 101010 elevado a la 0")
+numero = 101010
+elevado = 0
+resultado = numero ** elevado
+print(numero, "elevado a la : ", elevado, "es igual a: ", resultado)
+print('Hay un detalle cuando se eleva al 0 (cero)')
+
+
+print()
+#........................................................
+# Ejercicio resuelto N021
+# programa: Se desea saber cuanto es 101010 elevado a la 1
+# 
+print("Programa N021: Calcular 101010 elevado a la 1")
+numero = 101010
+elevado = 1
+resultado = numero ** elevado
+print(numero, "elevado a la : ", elevado, "es igual a: ", resultado)
+print('Hay un detalle cuando se eleva a la 1')
+
+print()
+
+# Primero cópialos como están escritos, luego intenta modificar sus valores para practicar
+#Rafael Aquino / mayo-2020 / pythoncumanes
