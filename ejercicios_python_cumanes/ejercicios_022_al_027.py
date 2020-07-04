eaeccc521aee161d134429da8c03ba9834c077c2
Rafael AQuino <46247548+siraquino@users.noreply.github.com>
2020-07-04 16:28:56 -0400

    Create ejercicios_022_al_027.py
    Nuevos ejercicios

---
+++ ejercicios_python_cumanes/ejercicios_022_al_027.py
@@ -0,0 +1,65 @@
+# Ejercicios resueltos del 022 al 027
+
+# Los ejercicios del 022 al 027 son de Regla de prioridad
+#......................................................
+# Ejercicio resuelto N022
+# programa: Calcular el promedio de 4 numeros: 34,56,23,10
+# 
+print("Programa N022: Calcular el promedio de: 34,56,23,10")
+promedio = (34 + 56 + 23 + 10)/4
+print("El promedio es: ",promedio)
+print()
+#...................................................
+
+# Ejercicio resuelto N023
+# programa: Se desea saber el resultado de la siguiente operación aritmética 
+# 34 + 56 + 23 + 10/4
+# 
+print("Programa N023: Calcular 34 + 56 + 23 + 10/4")
+calculo = 34 + 56 + 23 + 10/4 
+print(" El resultado de 34 + 56 + 23 + 10/4 es: ",calculo)
+print()
+#...................................................
+
+# Ejercicio resuelto N024
+# programa: Se desea saber el resultado de la siguiente operación aritmética 
+# 3 + 5 + (2*4)
+#
+print("Programa N024: Calcular 3 + 5 + (2*4)")
+x = 3 + 5 + (2*4)
+print(" El resultado de  3 + 5 + (2*4)  es: ",x)
+print()
+#...................................................
+
+# Ejercicio resuelto N025
+# programa: Se desea saber el resultado de la siguiente operación aritmética 
+# (3 + 5 + 2)*4
+#
+print("Programa N025: Calcular (3 + 5 + 2)*4")
+y = (3 + 5 + 2)*4
+print(" El resultado de (3 + 5 + 2)*4  es: ",y)
+print()
+#...................................................
+# Ejercicio resuelto N026
+# programa: Se desea saber el resultado de la siguiente operación aritmética 
+# 6 * 1 + 2
+#
+print("Programa N026: Calcular 6 * 1 + 2")
+op =  6 * 1 + 2
+print(" El resultado de  6 * 1 + 2  es: ",op)
+print()
+#...................................................
+# Ejercicio resuelto N027
+# programa: Se desea saber el resultado de la siguiente operación aritmética 
+# 6 * (1 + 2)
+#
+print("Programa N027: Calcular 6 * (1 + 2)")
+opera = 6 * (1 + 2)
+print(" El resultado de  6 * (1 + 2)  es: ",opera)
+print()
+#...................................................
+
+#
+
+# Primero cópialos como están escritos, luego intenta modificar sus valores para practicar
+#Rafael Aquino / mayo-2020 / pythoncumanes
