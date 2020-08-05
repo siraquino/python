dd4b451640c2d42af71d39cafbecfb5dd664647d
Rafael AQuino <46247548+siraquino@users.noreply.github.com>
2020-08-05 15:35:54 -0400

    Create ejercicios_070_al_071.py

---
+++ ejercicios_python_cumanes/ejercicios_070_al_071.py
@@ -0,0 +1,93 @@
+# Ejercicios resueltos del 070 al 071
+
+# Ejercicios varios tablas
+#.................................................
+
+# Ejercicio resuelto N070
+# Problema: Realizar la tabla de multiplicar del 9.
+# Primera Forma   
+print()
+print("Programa N070: tabla de multiplicar del 9")
+print("-"*40)
+n = 9
+s = '123456789' 
+x = int(s[0])
+print(n, " * ",x , " = ", n*x)
+x = int(s[1])
+print(n, " * ", x, " = ", n*x)
+x = int(s[2])
+print(n, " * ", x, " = ", n*x)
+x = int(s[3])
+print(n, " * ", x, " = ", n*x)
+x = int(s[4])
+print(n, " * ", x, " = ", n*x)
+x = int(s[5])
+print(n, " * ", x, " = ", n*x)
+x = int(s[6])
+print(n, " * ", x, " = ", n*x)
+x = int(s[7])
+print(n, " * ", x, " = ", n*x)
+x = int(s[8])
+print(n, " * ", x, " = ", n*x)
+# x = int(s[9])
+# print(n, " * ", x, " = ", n*x)
+print()
+#.............................................
+# Ejercicio resuelto N071
+# Problema: Tabla equivalencia del bolivar y Dollar.
+print("Programa N071: Tabla de conversiónn")
+print("-"*40)
+dollar = 260000
+s = '123456789' 
+x = int(s[0])
+print("Cant    Dollar     Bolivares")
+print(x, "   * ",dollar , " = ", dollar*x)
+x = int(s[1])
+print(x, "   * ", dollar, " = ", dollar*x)
+x = int(s[2])
+print(x, "   * ", dollar, " = ", dollar*x)
+x = int(s[3])
+print(x, "   * ", dollar, " = ", dollar*x)
+x = int(s[4])
+print(x, "   * ", dollar, " = ", dollar*x)
+x = int(s[5])
+print(x, "   * ", dollar, " = ", dollar*x)
+x = int(s[6])
+print(x, "   * ", dollar, " = ", dollar*x)
+x = int(s[7])
+print(x, "   * ", dollar, " = ", dollar*x)
+x = int(s[8])
+print(x, "   * ", dollar, " = ", dollar*x)
+print()
+print("Otra Forma...")
+print()
+s = '123456789' 
+x = int(s[0])
+print("Valor del Dollar: ", dollar)
+print("$     Bolivares")
+print(x,  " = ", dollar*x)
+x = int(s[1])
+print(x, " = ", dollar*x)
+x = int(s[2])
+print(x, " = ", dollar*x)
+x = int(s[3])
+print(x, " = ", dollar*x)
+x = int(s[4])
+print(x, " = ", dollar*x)
+x = int(s[5])
+print(x, " = ", dollar*x)
+x = int(s[6])
+print(x, " = ", dollar*x)
+x = int(s[7])
+print(x, " = ", dollar*x)
+x = int(s[8])
+print(x, " = ", dollar*x)
+#..............................................
+# Primero cópialos como están escritos, luego intenta modificar sus valores para practicar.
+#
+# A medida que practiquemos la creatividad se va desarrollando. La creatividad siempre ha
+# estado, tan solo debemos desarrollarla, fortalecerla y ponerla a nuestra disposición.
+#
+# Cuando avancemos verás otras formas de generar las tablas más eficientemente. Estas las generamos
+# con los conocimientos que tenemos hasta el momento y un poco de creatividad.
+#Rafael Aquino / junio-2020 / pythoncumanes
