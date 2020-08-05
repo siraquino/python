b4cd46ea177e6f1d9fda04a01cfbf7c95e7c0fbd
Rafael AQuino <46247548+siraquino@users.noreply.github.com>
2020-08-05 07:31:35 -0400

    Create ejercicios_065_al_069.py
    actualizando

---
+++ ejercicios_python_cumanes/ejercicios_065_al_069.py
@@ -0,0 +1,80 @@
+# Ejercicios resueltos del 065 al 069
+
+# Ejercicios del 065 al 069: Ejercicios varios
+#.................................................
+
+# Ejercicio resuelto N065
+# Problema: Visualizar por pantalla los números del 1 al 5.
+# Primera Forma   
+print()
+print("Programa N065: Visualizar por pantalla los números del 1 al 5")
+print("Primera forma")
+numeros = '1 2 3 4 5' 
+print("Números del 1 al 5: " + numeros)
+print()
+#.............................................
+# Ejercicio resuelto N066
+# Problema: Visualizar por pantalla los números del 1 al 5.
+# Segunda Forma 
+print()
+print("Programa N066: Visualizar por pantalla los números del 1 al 5")
+print("Segunda Forma")
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+#..............................................
+# Ejercicio resuelto N067
+# Problema: Visualizar por pantalla los números del 1 al 5.
+# Tercera Forma 
+print()
+print("Programa N067: Visualizar por pantalla los números del 1 al 5")
+print("Tercera Forma")
+print('1')
+print('2')
+print('3')
+print('4')
+print('5')
+#..............................................
+# Ejercicio resuelto N068
+# Problema: Visualizar por pantalla los números del 1 al 5.
+# Cuarta Forma 
+print()
+print("Programa N068: Visualizar por pantalla los números del 1 al 5")
+print("Cuarta Forma")
+uno = 1
+dos = 2
+tres = 3
+cuatro = 4
+cinco = 5
+print(uno) 
+print(dos) 
+print(tres) 
+print(cuatro) 
+print(cinco) 
+#..............................................
+print ()
+# Ejercicio resuelto N069
+# Problema: Visualizar por pantalla los números del 1 al 5.
+# Quinta Forma 
+print()
+print("Programa N069: Visualizar por pantalla los números del 1 al 5")
+print("Quinta Forma")
+uno = 1
+dos = uno + 1
+tres = dos + 1
+cuatro = tres + 1
+cinco = cuatro + 1
+print(uno) 
+print(dos) 
+print(tres) 
+print(cuatro) 
+print(cinco)
+#..............................................
+# Primero cópialos como están escritos, luego intenta modificar sus valores para practicar.
+# A medida que practiquemos la creatividad se va desarrollando. La creatividad siempre ha
+# estado, tan solo debemos desarrollarla, formatecerla y ponerla a nuestra disposición.
+# Cuando avancemos verás otras formas de generar números más eficiente. Estos los generamos
+# con los conocimientos que tenemos hasta el momento.
+#Rafael Aquino / junio-2020 / pythoncumanes
