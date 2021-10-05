bff7b12469d7bbc832a3bdee5d263f945d8ac051
Rafael AQuino <46247548+siraquino@users.noreply.github.com>
2021-10-05 17:17:03 -0400

    Update ejercicios_151_al_155.py

--- ejercicios_python_cumanes/ejercicios_151_al_155.py
+++ ejercicios_python_cumanes/ejercicios_151_al_155.py
@@ -16,66 +16,7 @@ while i <= 10:
     i = i + 1
 print(lista)
 
-#.................................................
-# Ejercicio resuelto N152
 
-print()
-print("-"*44)
-print("Programa N152: Ordenar lista  = [10, 2, 100, 4, 55, 9, 1]")
-print("-"*44)
-# Ejercicio para ordenar los elementos de una lista
-lista = [10, 2, 100, 4, 55, 9, 1]
-print("Lista Original")
-print(lista)
-print("Lista Ordenada")
-lista.sort()
-print(lista)
-
-#.................................................
-# Ejercicio resuelto N153
-
-print()
-print("-"*44)
-print("Programa N153: Invertir lista  = [100, 200, 300, 400, 500, 600, 700]")
-print("-"*44)
-# Ejercicio para Invertir los elementos de una lista
-lista = [100, 200, 300, 400, 500, 600, 700]
-print("Lista Original")
-print(lista)
-print("Lista Invertida")
-lista.reverse()
-print(lista)
-
-#.................................................
-# Ejercicio resuelto N154
-
-print()
-print("-"*44)
-print("Programa N154: insertar elemento lista  = [100, 200, 300, 400, 500, 600, 700]")
-print("en la posicion indicada")
-print("-"*44)
-# Ejercicio para insertar elemento en una posicion dada
-lista = [100, 200, 300, 400, 500, 600, 700]
-print("Lista Original")
-print(lista)
-print("Insertar el 55 en la posicion 3")
-lista.insert(3,55)
-print(lista)
-#.................................................
-# Ejercicio resuelto N155
-
-print()
-print("-"*44)
-print("Programa N155: Eliminar el ultimo elemento de lista")
-print("lista  = [100, 200, 300, 400, 500, 600, 700]")
-print("-"*44)
-# Ejercicio para insertareliminar elemento final
-lista = [100, 200, 300, 400, 500, 600, 700]
-print("Lista Original")
-print(lista)
-print("Eliminado el ultimo elemento")
-lista.pop()
-print(lista)
   
 
 # Recuerda Mucha Creatividad, Mucha Creatividad!
